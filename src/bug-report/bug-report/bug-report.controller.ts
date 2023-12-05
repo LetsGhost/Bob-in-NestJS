153c92bcd53be4f1290ab8100fc0cd894260ff83
Paul Ehrling <ghostlest330@gmail.com>
2023-12-05 08:49:40 +0100

    Update

--- src/bug-report/bug-report/bug-report.controller.ts
+++ src/bug-report/bug-report/bug-report.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, Patch, Param } from '@nestjs/common';
 import { BugReportService } from './bug-report.service';
 
 @Controller('bug-report')
@@ -14,6 +14,12 @@ export class BugReportController {
   @Get()
   async findAll() {
     const result = await this.bugReportService.findAll();
+    return result;
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateBugReportDto: any) {
+    const result = await this.bugReportService.update(id, updateBugReportDto);
     return result;
   }
 }
